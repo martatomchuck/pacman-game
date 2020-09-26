cd578243ccc54022858e9da240e59db01ca4eb65
Marta Tomczak <49766582+mtomchuck@users.noreply.github.com>
2020-09-26 11:23:06 +0200

    basic styles

--- src/components/Pacman.js
+++ src/components/Pacman.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import '../css/pacman.css';
+import Header from '../components/Header';
+import Board from '../components/Board';
 
 function Pacman() {
+  const [score, setScore] = useState(0);
+
   return (
     <div>
-       Pacman
+        <Header score={score}/>
+        <Board/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './components/Pacman';
+import Pacman from './components/Pacman';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Pacman />
   </React.StrictMode>,
   document.getElementById('root')
 );
