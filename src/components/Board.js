a80a0c61ae0fedafe91488d08c3ac96fa7544139
Marta Tomczak <49766582+mtomchuck@users.noreply.github.com>
2020-09-27 23:22:04 +0200

    adding Food component

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,22 +2,33 @@ import React, { Component } from 'react';
 
 import Pacman from './Pacman';
 import Ghost from './Ghost';
+import Food from './Food';
 
 class Board extends Component {
+
+    constructor(props) {
+        super(props);
+        this.amountOfFood = 
+    }
+
     render() {
         return (
             <div className="board">
                 <Pacman/>
                 <Ghost color="blue"/>
                 <Ghost color="pink"/>
-                {/* <Food/> */}
+                <Food position={{top: 100, left: 100}}/>
+                <Food position={{top: 150, left: 100}}/>
             </div>
         )
     }
 }
 
+// TO DO move to config
 Board.defaultProps = {
-    
+    foodSize: 50,
+    border: 10 * 2,
+    topScoreHeight: 60
 };
 
 export default Board;
