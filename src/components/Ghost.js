ae78f9efc19718aacd1d24fb37c4f9b87ac81ef3
Marta Tomczak <49766582+mtomchuck@users.noreply.github.com>
2020-09-27 23:03:59 +0200

    moving ghosts

--- src/components/Ghost.js
+++ src/components/Ghost.js
@@ -22,15 +22,64 @@ class Ghost extends Component {
     }
 
     componentDidMount() {
-        this.changeDirectionInterval = setInterval(this.changeDirection, 20000);
+        this.changeDirectionInterval = setInterval(this.changeDirection, 2000);
+        this.moveInterval = setInterval(this.move, 2000);
     }
 
     componentWillUnmount() {
         clearInterval(this.changeDirectionInterval);
+        clearInterval(this.moveInterval);
     }
 
     move = () => {
-        
+        const currentTop = this.state.position.top;
+        const currentLeft = this.state.position.left;
+        const { direction } = this.state;
+        const { step, border, size, topScoreHeight } = this.props;
+
+        // 37 ArrowLeft
+        // 38 ArrowUp
+        // 39 ArrowRight
+        // 40 ArrowDown
+
+        switch (direction) {
+            case 'left':
+                this.setState({
+                    position: {
+                        top: currentTop,
+                        // if the value of left is less than 0, take 0
+                        left: Math.max(currentLeft - step, 0)
+                    }
+                })
+                break;
+            case 'up':
+                this.setState({
+                    position: {
+                        // if the value of top is less than 0, take 0
+                        top: Math.max(currentTop - step, 0),
+                        left: currentLeft
+                    }
+                })
+                break;
+            case 'right':
+                this.setState({
+                    position: {
+                        top: currentTop,
+                        // minimal value of: right step or window width minus border and pacman size)
+                        left: Math.min(currentLeft + step, window.innerWidth - border - size - 80)
+                    }
+                })
+                break;
+            case 'down':
+                this.setState({
+                    position: {
+                        top: Math.min(currentTop + step, window.innerHeight - border - size - topScoreHeight - 60),
+                        left: currentLeft
+                    }
+                })
+                break;
+            default:
+        }
     }
 
     render() {
